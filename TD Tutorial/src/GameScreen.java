ec515cdf585cd39e076b0222edec569f4def53d3
nabingx <nngocanh697@gmail.com>
2022-07-05 15:33:27 +0700

    Update

--- TD Tutorial/src/GameScreen.java
+++ TD Tutorial/src/GameScreen.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -7,20 +8,35 @@ import javax.swing.JPanel;
 public class GameScreen extends JPanel{
 
 	private Random random;
+	private BufferedImage img;
 	
-	public GameScreen() {
+	public GameScreen(BufferedImage img) {
+		this.img = img;
 		random = new Random();
 	}
 	
 	public void paintComponent(Graphics g) {
-		
 		super.paintComponent(g);
 		
-		for (int x = 0; x < 20; x++) {
-			g.setColor(new Color(105, 10, 50));
-			g.fillRect(x * 32, 0, 32, 32);
-		}
 		
+		g.drawImage(img, 0, 0, null);
+		
+		
+//		for (int y = 0; y < 20; y++) {
+//			for (int x = 0; x < 20; x++) {
+//				g.setColor(getRndColor());
+//				g.fillRect(x * 32, y * 32, 32, 32);
+//			}
+//		}
+		
+	}
+	
+	private Color getRndColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		
+		return new Color(r, g, b);
 	}
 	
 }
