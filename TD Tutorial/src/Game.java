c643d5f9f18b67fd748a4433a16d23acf5855ae7
nabingx <nngocanh697@gmail.com>
2022-07-05 14:38:58 +0700

    Update

--- TD Tutorial/src/Game.java
+++ TD Tutorial/src/Game.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import javax.swing.JFrame;
-
-public class Game extends JFrame{
-	
-	private GameScreen gameScreen;
-	
-	public Game() {
-		
-		setSize(400, 400);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
-		
-		gameScreen = new GameScreen();
-		add(gameScreen);
-		
-	}
-
-	public static void main(String[] args) {
-		
-		System.out.println("Start of my TD Tutorial. Hello there!");
-		
-		Game game = new Game();
-
-	}
-
-}
-=======
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
@@ -54,4 +25,3 @@ public class Game extends JFrame{
 	}
 
 }
->>>>>>> 4e06bdb94f976f6530eb0adc54837f9091b4350b
--- TD Tutorial/src/GameScreen.java
+++ TD Tutorial/src/GameScreen.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -21,27 +20,3 @@ public class GameScreen extends JPanel{
 	}
 	
 }
-=======
-import java.awt.Color;
-import java.awt.Graphics;
-
-import javax.swing.JPanel;
-
-public class GameScreen extends JPanel{
-
-	public GameScreen() {
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-		
-		super.paintComponent(g);
-		
-		g.setColor(Color.RED);
-		g.fillRect(50, 50, 100, 100);
-		//g.drawRect(50, 50, 100, 100);
-		
-	}
-	
-}
->>>>>>> 4e06bdb94f976f6530eb0adc54837f9091b4350b
